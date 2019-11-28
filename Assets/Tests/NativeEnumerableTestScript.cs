a00bd771f4f44304b0cb590378945c051d70f471
pcysl5edgo <pcysl5edgo@gmail.com>
2019-11-28 21:20:00 +0900

    More Test

--- Assets/Tests/NativeEnumerableTestScript.cs
+++ Assets/Tests/NativeEnumerableTestScript.cs
@@ -10,44 +10,42 @@ namespace Tests
       
     }
 
-    // [TestCase(0L)]
-    // [TestCase(-10L)]
-    // [TestCase(-12241L)]
+    [TestCase(0L)]
+    [TestCase(-10L)]
+    [TestCase(-12241L)]
     [TestCase(long.MinValue)]
     public void ZeroOrNegativeCountTest(long a)
     {
       
     }
 
-    // [TestCase(0, Allocator.Temp)]
-    // [TestCase(1, Allocator.Temp)]
-    // [TestCase(10, Allocator.Temp)]
-    // [TestCase(114514, Allocator.Temp)]
-    // [TestCase(0, Allocator.TempJob)]
-    // [TestCase(1, Allocator.TempJob)]
-    // [TestCase(10, Allocator.TempJob)]
-    // [TestCase(114514, Allocator.TempJob)]
-    // [TestCase(0, Allocator.Persistent)]
-    // [TestCase(1, Allocator.Persistent)]
-    // [TestCase(10, Allocator.Persistent)]
-    // [TestCase(114514, Allocator.Persistent)]
-    [Test]
-    public void FromNativeArrayPass()
+    [TestCase(0, Allocator.Temp)]
+    [TestCase(1, Allocator.Temp)]
+    [TestCase(10, Allocator.Temp)]
+    [TestCase(114514, Allocator.Temp)]
+    [TestCase(0, Allocator.TempJob)]
+    [TestCase(1, Allocator.TempJob)]
+    [TestCase(10, Allocator.TempJob)]
+    [TestCase(114514, Allocator.TempJob)]
+    [TestCase(0, Allocator.Persistent)]
+    [TestCase(1, Allocator.Persistent)]
+    [TestCase(10, Allocator.Persistent)]
+    [TestCase(114514, Allocator.Persistent)]
+    public void FromNativeArrayPass(int count, Allocator allocator)
     {
       
     }
 
-    // [TestCase(0, Allocator.Temp)]
-    // [TestCase(114, Allocator.Temp)]
-    // [TestCase(114514, Allocator.Temp)]
-    // [TestCase(0, Allocator.TempJob)]
-    // [TestCase(114, Allocator.TempJob)]
-    // [TestCase(114514, Allocator.TempJob)]
-    // [TestCase(0, Allocator.Persistent)]
-    // [TestCase(114, Allocator.Persistent)]
-    // [TestCase(114514, Allocator.Persistent)]
-    [Test]
-    public void IEnumerableTest()
+    [TestCase(0, Allocator.Temp)]
+    [TestCase(114, Allocator.Temp)]
+    [TestCase(114514, Allocator.Temp)]
+    [TestCase(0, Allocator.TempJob)]
+    [TestCase(114, Allocator.TempJob)]
+    [TestCase(114514, Allocator.TempJob)]
+    [TestCase(0, Allocator.Persistent)]
+    [TestCase(114, Allocator.Persistent)]
+    [TestCase(114514, Allocator.Persistent)]
+    public void IEnumerableTest(int count, Allocator allocator)
     {
       
     }
