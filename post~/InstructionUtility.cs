91b5b2adfa1aedabf13bf2bb2150775b4aab2e3c
pcysl5edgo <pcysl5edgo@gmail.com>
2019-11-28 21:22:05 +0900

    [add]post~ dll folder

---
+++ post~/InstructionUtility.cs
@@ -0,0 +1,5 @@
+﻿using Mono.Cecil.Cil;
+internal static class InstructionUtility
+{
+  public static void Replace(this Instruction instruction, Instruction replace) => (instruction.OpCode, instruction.Operand) = (replace.OpCode, replace.Operand);
+}
