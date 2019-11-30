968095a9ddf1ddf2c2a8bb435859b44e6857fa20
pcysl5edgo <pcysl5edgo@gmail.com>
2019-11-30 15:12:21 +0900

    [update]Unity Package

--- Assets/Editor/Executor.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public static class Z
-{
-    public static void Hoge()
-    {
-       System.IO.File.WriteAllText("a.txt", "hello world");
-    }
-}
---
+++ Assets/Editor/UnityPackageBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEditor;
+
+public static class UnityPackageBuilder
+{
+    public static void Build()
+    {
+        const string exportPath = "../UniNativeLinq.unitypackage";
+        AssetDatabase.ExportPackage(
+           new []{
+               "Assets/Plugins/UNL/UniNativeLinq.dll"
+           },
+           exportPath,
+           ExportPackageOptions.Default
+        );
+    }
+}
